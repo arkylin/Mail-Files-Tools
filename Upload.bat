de6ef2bf0554ed73ee1ab7dd16c983069ff33feb
旋风凌 <x@xyz.blue>
2020-01-07 19:51:18 +0800

    C

---
+++ Upload.bat
@@ -0,0 +1,8 @@
+@echo off
+set body="New Upload Files"
+set s="New Upload Files"
+set t=#收件邮箱#
+set f=#发件邮箱#
+set pw=#smtp密码#
+set attach=*.*
+Blat -body %body% -s %s% -attach %attach% -t %t% -server #smtp服务器# -f %f% -u %f% -pw %pw%
